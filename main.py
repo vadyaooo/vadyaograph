2246007873d69dfe3f9d361881b0da868dae31d2
Vadya <leonidovich1998@gmail.com>
2024-01-30 01:49:26 +0300

    Нормальная работа с открытием двух окон

--- main.py
+++ main.py
@@ -8,7 +8,18 @@ def main(page: ft.Page):
         u_la.value = 'Yes'
         page.update()
 
-    def man(page1: ft.Page):
+    
+
+    def man(page1 : ft.Page):
+
+        def ontablo(e):
+            page1.bgcolor = 'white'
+            page1.update()
+
+        def onschet(e):
+            page1.bgcolor = 'red'
+            page1.update()
+
         page1.title = 'efe'
         page1.bgcolor = 'green'
         page1.add(
@@ -19,9 +30,7 @@ def main(page: ft.Page):
             )
         )
 
-    def ontablo(e):
-        page.title = "trg"
-        page.bgcolor = 'white'
+        
         page.add(
             ft.Row(
                 [
@@ -29,11 +38,11 @@ def main(page: ft.Page):
                 ]
             ),
             ft.Row([
-                ft.OutlinedButton(text="Показать табло", on_click=ontablo)
+                ft.OutlinedButton(text="Показать табло", on_click=ontablo),
+                ft.OutlinedButton(text="Показать счет", on_click=onschet)
             ])
         )
         # ft.app(port=8550, target=man, view=ft.AppView.WEB_BROWSER)
-        ft.app(target=man)
-
+    ft.app(target=man)
 
 ft.app(target=main)
